5e378a6353d5fd63ad67df976c2b0c1548011f10
Oskar <oveggeland@gmail.com>
2021-06-23 17:16:47 +0200

    visualize functions for clustering algorithms

--- visualizer.py
+++ visualizer.py
@@ -8,3 +8,20 @@ def draw_circles(img, centers, radius=5, linesize=3):
         center = (centers[i, 1], centers[i, 0])
         cv.circle(img, center, radius, (0, 255, 0), linesize)
     cv.imshow('Circles', img)
+    return img
+
+
+def draw_db_clusters(img, coordinates, db):
+    n_clusters = np.max(db.labels_)+1
+    for c in range(n_clusters):   # Iterate over each cluster
+        cluster_indices = np.where(db.labels_ == c)[0]
+        cluster_coordinates = coordinates[cluster_indices].astype('int')
+        for i in range(cluster_indices.size):
+            y = int(cluster_coordinates[i, 0])
+            x = int(cluster_coordinates[i, 1])
+            img[y, x] = 127
+        cv.imshow(f"num{c}", img)
+        key = cv.waitKey()
+        cv.destroyAllWindows()
+        if key & 0xFF == ord('q'):
+            exit()
