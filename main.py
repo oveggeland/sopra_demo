406ca9ec58e324dee5d89b0372aa110b36a96f51
Oskar <oveggeland@gmail.com>
2021-06-28 14:36:32 +0200

    implement strategy to calculate number of chickens by white pixel area

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ from video_reader import read_video
 
 if __name__ == '__main__':
     #read_video()
-    chickens_over_time()
-    find_empty_spaces()
-    create_annotation_images()
+    chickens_over_time('area')
+    #find_empty_spaces()
+    #create_annotation_images()
--- sandbed.py
+++ sandbed.py
@@ -22,17 +22,20 @@ INNER_BED_CORNERS = np.array([
 
 VIDEO_NUM = 1
 IMG_NUM = 1
-N_IMAGES = 5
-IMAGE_INTERVAL = 1000
+N_IMAGES = 50
+IMAGE_INTERVAL = 100
 FRAME_TIME = 1/19.72
 
 
-def chickens_over_time(video_num=VIDEO_NUM, img_num=IMG_NUM, n_images=N_IMAGES,\
+def chickens_over_time(strategy='clusters', video_num=VIDEO_NUM, img_num=IMG_NUM, n_images=N_IMAGES,\
                        image_interval=IMAGE_INTERVAL, frame_time=FRAME_TIME):
     chicken_counter = []
     time = []
     for i in range(n_images):
-        n_chickens = find_chickens(video_num, img_num+image_interval*i)
+        if strategy=='clusters':
+            n_chickens = find_chickens_by_clusters(video_num, img_num+image_interval*i)
+        else:
+            n_chickens = find_chickens_by_area(video_num, img_num+image_interval*i)
 
         chicken_counter.append(n_chickens)
         time.append((img_num+image_interval*i)*frame_time)
@@ -41,9 +44,31 @@ def chickens_over_time(video_num=VIDEO_NUM, img_num=IMG_NUM, n_images=N_IMAGES,\
     plt.show()
 
 
-def find_chickens(vid_num, img_num):
+def find_chickens_by_area(vid_num, img_num):
     dir = f'data/video{vid_num}/img{img_num}.jpg'
-    print(dir)
+    img = cv.imread(dir)
+    img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    img = crop_image(img, OUTER_BED_CORNERS)
+
+    # Blur and threshold image
+    blurred_img = cv.bilateralFilter(img, 20, 20, 20)
+    threshold_img = cv.adaptiveThreshold(blurred_img, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 101, -10)
+
+    total_area = img.size
+    white_area = np.where(threshold_img)[0].size
+    white_ratio = white_area/total_area
+
+    average_chicken_size = 4000
+    n_chickens = white_area // average_chicken_size
+    #cv.imshow(f'Number of chickens by area approximation: {n_chickens}', img)
+    #cv.waitKey()
+    #cv.destroyAllWindows()
+
+    return n_chickens
+
+
+def find_chickens_by_clusters(vid_num, img_num):
+    dir = f'data/video{vid_num}/img{img_num}.jpg'
     img = cv.imread(dir)
     img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
     masked_img = mask_image(img, INNER_BED_CORNERS)
