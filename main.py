9749b8a9ff5d2e97a5ffd2db3310e9f341fb4c4e
Oskar <oveggeland@gmail.com>
2021-06-28 13:34:57 +0200

    ready for pull-request

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
-import cv2 as cv
-import numpy as np
-
-from visualizer import draw_circles
 from annotation import create_annotation_images
 
 
-def main():
-    create_annotation_images()        # Read video and save individual images
-
-
 if __name__ == '__main__':
-    main()
+    create_annotation_images()        # Read video and save individual images
