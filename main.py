5564e62a2baab7a5c6302c51ce212fccef56fefb
Oskar <oveggeland@gmail.com>
2021-06-28 11:13:37 +0200

    annotation image crop script

--- main.py
+++ main.py
@@ -1,61 +1,12 @@
 import cv2 as cv
 import numpy as np
 
-from clustering import kmeans_cluster
 from visualizer import draw_circles
-
-# Specify what image/frame to analyze
-VIDEO_NUM = 1
-IMG_NUM = 1
-N_IMAGES = 5
+from annotation import create_annotation_images
 
 
 def main():
-    read_video()        # Read video and save individual images
-    analyze_image()     # Analyze image (default is specified by VIDEO_NUM and IMG_NUM)
-
-
-def analyze_image(dir=f'data/video{VIDEO_NUM}/img{IMG_NUM}.jpg'):
-    img = cv.imread(dir)
-    img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-    img = cv.resize(img, (1024, 720))  # TODO: Move this
-
-    # Smooth images
-    blurred_img = cv.bilateralFilter(img, 20, 20, 10)
-
-    # Apply white/black thresholding
-    threshold_img = cv.adaptiveThreshold(blurred_img, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 51, -10)
-
-    # Cluster white areas to detect hens
-    centers = kmeans_cluster(threshold_img, k=300)
-
-    # Show different results results
-    draw_circles(img, centers, radius=20)       # Detection visualizer
-    #cv.imshow('Frame', img)
-    #cv.imshow('Blurred', blurred_img)
-    #cv.imshow('Threshold', threshold_img)
-
-    # Press any button to move on. Pressing 'q' exits the entire script
-    key = cv.waitKey()
-    if key & 0xFF == ord('q'):
-        exit()
-
-
-def read_video(filename=f"data/video{VIDEO_NUM}/video{VIDEO_NUM}.mp4"):
-    cap = cv.VideoCapture(filename)
-    assert cap.isOpened(), "Error streaming video"
-
-    counter = 1
-    n_images = N_IMAGES
-    while cap.isOpened():
-        retval, frame = cap.read()
-        if retval:
-            frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)  # Bruker 3 ganger mindre plass
-            cv.imwrite(f'data/video{VIDEO_NUM}/img{counter}.jpg', frame)
-            if counter >= n_images:
-                break
-        counter += 1
-    cap.release()
+    create_annotation_images()        # Read video and save individual images
 
 
 if __name__ == '__main__':
