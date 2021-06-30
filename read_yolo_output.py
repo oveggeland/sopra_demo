be1d04a45cc51ba3f347b063fbd3a43d5ca0ce44
ahofman <aurora.hofman@soprasteria.no>
2021-06-30 11:58:40 +0200

    add as_pixel to return_center

--- read_yolo_output.py
+++ read_yolo_output.py
@@ -3,13 +3,17 @@ import pandas as pd
 import cv2 as cv
 from image_utils import mask_image
 
-def return_center(path = "yolo/runs/detect/exp/labels/1001_jpg.rf.5746260f252219fdb0f254e167edacc2.txt"):
+def return_center(path = "yolo/runs/detect/exp/labels/1001_jpg.rf.5746260f252219fdb0f254e167edacc2.txt", as_pixel = TRUE):
     """Return center of yolo detected objects as a numpy array with x, y coordinates"""
     data = pd.read_csv(path, sep=" ", header=None)
     data.columns = ["class", "x", "y", "w", "h."]
+    center_array = data.to_numpy()[:, 1:3]
+    if as_pixel:
+        center_array = center_array * [w, h]
+    return(center_array)
 
-    return(data.to_numpy()[:, 1:3])
-
+def convert_norm_to_pixel(center_array, w, h):
+    center_array * [w, h]
 #coutn in box
 
 corners = np.array([
