3f9ce68b8af7c2e5b4398607cfd647352bbcdd81
Oskar <oveggeland@gmail.com>
2021-06-30 15:20:02 +0200

    improve corner_request gui

--- gui.py
+++ gui.py
@@ -5,22 +5,26 @@ import numpy as np
 def draw_circle(event, x, y, flags, param):
     global mouseX, mouseY
     if event == cv.EVENT_LBUTTONDOWN:
-        cv.circle(param[0], (x, y), 20, (255, 0, 255), 5)
+        cv.circle(param[0], (x, y), 20, (0, 255, 0), 5)
         mouseX, mouseY = x, y
         print(f'You clicked on ({x}, {y})')
         param[1].append(np.array([x, y]))
 
 
-def request_corners(img):
+def request_corners(img, num=None):
     cv.namedWindow('image')
     corners = []
-    cv.setMouseCallback('image', draw_circle, (img, corners))
+    cv.setMouseCallback('image', draw_circle, [img, corners])
 
-    print("Double click on corners. To finish, please press q button :) ")
+    print(f"Click on corners. z button regrets the last corner. q to finish.")
     while True:
         cv.imshow('image', img)
         k = cv.waitKey(20) & 0xFF
-        if k == ord('q'):
+        if k == ord('q') or len(corners) == num:
             break
+        if k == ord('z'):
+            del corners[-1]
+            print(corners)
+            cv.circle(img, (mouseX, mouseY), 20, (0, 0, 255), 5)
 
     return np.array(corners)
